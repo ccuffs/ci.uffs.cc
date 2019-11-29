06b11b315314fe062ef1860216b33994725472d5
Fernando Bevilacqua <dovyski@gmail.com>
2019-11-29 14:28:38 -0300

    Estrutura inicial

---
+++ index.php
@@ -0,0 +1,139 @@
+<html>
+<head>
+    <title>CI - Ciência da Computação - UFFS</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i">
+
+    <link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/froala_blocks.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/app.css">
+</head>
+
+<body id="top">
+    <header class="bg-dark">
+        <div class="container">
+            <nav class="navbar navbar-expand-md no-gutters">
+                <div class="col-2 text-left">
+                    <i class="fa fa-warning"></i> ci.uffs.cc
+                </div>
+
+                <div class="collapse navbar-collapse justify-content-center col-8" id="navbarNav4">
+                    <ul class="navbar-nav justify-content-center">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="https://www.froala.com">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <ul class="navbar-nav col-2 justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.froala.com"><i class="fa fa-github"></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="panel">
+                    <div class="panel-body">
+                        <h3><i class="fa fa-warning"></i> Metrics </h3>
+                        <hr>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <hr>
+                        <div class="row">
+                            <div class="col-3">
+                                <small>Today</small><br />
+                                <strong>20.13 <i class="fa fa-user"></i></strong>
+                            </div>
+                            <div class="col-3">
+                                <small>Last</small><br />
+                                <strong>20.73 <i class="fa fa-user"></i></strong>
+                            </div>
+                            <div class="col-md-3 col-xs-6">
+                                <small>Last</small><br />
+                                <strong>20.53 <i class="fa fa-user"></i></strong>
+                            </div>
+                            <div class="col-md-3 col-xs-6">
+                                <small>Something</small><br />
+                                <strong>20.24 <i class="fa fa-user"></i></strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="panel panel-filled">
+                    <div class="panel-heading">
+                        <h2>Something</h2>
+                    </div>
+                    <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+                        <table id="tableServices" class="table table-striped table-hover table-responsive-sm no-footer"
+                            role="grid">
+                            <thead>
+                                <tr role="row">
+                                    <th tabindex="0">Name</th>
+                                    <th tabindex="0">Position</th>
+                                    <th tabindex="0">Position</th>
+                            </thead>
+
+                            <tbody>
+                                <tr role="row" class="odd">
+                                    <td>Airi Satou</td>
+                                    <td>Accountant</td>
+                                    <td>Tokyo</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="fdb-block footer-large bg-dark">
+        <div class="container">
+            <div class="row align-items-top text-center text-md-left">
+                <div class="col-12 col-sm-6 col-md-4">
+                    <h3>Country A</h3>
+                    <p>Street Address 52<br>Contact Name</p>
+                    <p>+44 827 312 5002</p>
+                    <p><a href="https://www.froala.com">countrya@amazing.com</a></p>
+                </div>
+
+                <div class="col-12 col-sm-6 col-md-4 mt-4 mt-sm-0">
+                    <h3>Country B</h3>
+                    <p>Street Address 100<br>Contact Name</p>
+                    <p>+13 827 312 5002</p>
+                    <p><a href="https://www.froala.com">countryb@amazing.com</a></p>
+                </div>
+
+                <div class="col-12 col-md-4 mt-5 mt-md-0 text-md-left">
+                    <h3>About Us</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
+                        live the blind texts.</p>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col text-center">
+                    <p class="muted">© 2019 All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
